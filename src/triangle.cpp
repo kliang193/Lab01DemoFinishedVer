ff5f8b563bea8077fa77eb04e8f1fe08f2c03c65
kliang193 <liangkent@yahoo.com>
2024-01-12 17:05:49 -0800

    uploading the rest of files

---
+++ src/triangle.cpp
@@ -0,0 +1,13 @@
+#include "../header/Triangle.hpp"
+
+void Triangle::set_base(int b) {
+    this->base = b;
+}
+
+void Triangle::set_height(int h) {
+    this->height = h;
+}
+
+int Triangle::area() {
+    return this->base * this->height * 1/2;
+}
